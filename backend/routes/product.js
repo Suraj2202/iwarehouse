7854e6325240b9baaeb01c8c7c5afd816cd8f88f
suraj2202 <suraj1.verma7@gmail.com>
2024-06-23 01:51:09 +0530

    skeleton backend done

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -45,12 +45,11 @@ router.post(
         description,
         price,
         buycount,
-        offercount
+        offercount,
       });
       const saveProduct = await product.save();
 
       res.status(200).json(saveProduct);
-
     } catch (exception) {
       console.log(exception.message);
       res.status(500).send("Internal Server Error");
@@ -60,50 +59,98 @@ router.post(
 
 //API 3: "api/product/updateproduct/:id" - Put update product = Login required
 router.put(
-    "/updateproduct/:id",
-    fetchUserId,
-    [
-      body("title", "Title must be atleast 3 character").isLength({ min: 3 }),
-      body("description", "Description must be atleast 5 character").isLength({
-        min: 5,
-      }),
-      body("price", "Price cannot be blank").exists(),
-      body("buycount", "Buy Count cannot be blank").exists(),
-      body("offercount", "Offer Count cannot be blank").exists(),
-    ],
-    async (req, res) => {
-      //validation : 400 - Bad Request
-      const result = validationResult(req);
-      if (!result.isEmpty()) {
-        return res.status(400).json({ errors: result.array() });
-      }
-  
-      try {
-        const { title, description, price, buycount, offercount } = req.body;
-  
-        const newProduct = {};
-
-        if(title){newProduct.title = title};
-        if(description){newProduct.description = description};
-        if(price){newProduct.price = price};
-        if(buycount){newProduct.buycount = buycount};
-        if(offercount){newProduct.offercount = offercount};
-
-        //Find the product to be updated by product id, not by user id
-        let product = await productModel.findById(req.params.id);
-        
-        if(!product){return res.status(404).send("Not Found")};
-
-        if(product.user.toString() !== req.user.id){return res.status(401).send("Unauthorized access, not allowed.")}
-        console.log(req.params.id)
-        product = await productModel.findByIdAndUpdate(req.params.id, {$set: newProduct}, {new:true});
-        res.json({product});
-      } catch (exception) {
-        console.log(exception.message);
-        res.status(500).send("Internal Server Error");
-      }
+  "/updateproduct/:id",
+  fetchUserId,
+  [
+    body("title", "Title must be atleast 3 character").isLength({ min: 3 }),
+    body("description", "Description must be atleast 5 character").isLength({
+      min: 5,
+    }),
+    body("price", "Price cannot be blank").exists(),
+    body("buycount", "Buy Count cannot be blank").exists(),
+    body("offercount", "Offer Count cannot be blank").exists(),
+  ],
+  async (req, res) => {
+    //validation : 400 - Bad Request
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(400).json({ errors: result.array() });
     }
-  );
-  
+
+    try {
+      const { title, description, price, buycount, offercount } = req.body;
+
+      const newProduct = {};
+
+      if (title) {
+        newProduct.title = title;
+      }
+      if (description) {
+        newProduct.description = description;
+      }
+      if (price) {
+        newProduct.price = price;
+      }
+      if (buycount) {
+        newProduct.buycount = buycount;
+      }
+      if (offercount) {
+        newProduct.offercount = offercount;
+      }
+
+      //Find the product to be updated by product id, not by user id
+      let product = await productModel.findById(req.params.id);
+
+      if (!product) {
+        return res.status(404).send("Not Found");
+      }
+
+      if (product.user.toString() !== req.user.id) {
+        return res.status(401).send("Unauthorized access, not allowed.");
+      }
+      console.log(req.params.id);
+      product = await productModel.findByIdAndUpdate(
+        req.params.id,
+        { $set: newProduct },
+        { new: true }
+      );
+      res.json({ product });
+    } catch (exception) {
+      console.log(exception.message);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
+
+//API 4: "api/product/deleteproduct/:id" - Delete delete product = Login required
+router.delete("/deleteproduct/:id", fetchUserId, async (req, res) => {
+  //validation : 400 - Bad Request
+  const result = validationResult(req);
+  if (!result.isEmpty()) {
+    return res.status(400).json({ errors: result.array() });
+  }
+
+  try {
+    //Find the product to be delete by product id, not by user id
+    let product = await productModel.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).send("Not Found");
+    }
+
+    if (product.user.toString() !== req.user.id) {
+      return res.status(401).send("Unauthorized access, not allowed.");
+    }
+    console.log(req.params.id);
+    product = await productModel.findByIdAndDelete(req.params.id);
+    res.json({
+        title: product.title,
+        "Success": "Product Deleted successfully." 
+    });
+  } catch (exception) {
+    console.log(exception.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 module.exports = router;
